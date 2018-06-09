df6d7963f34585dfe5597004beceadc8b34b144f
rpereyra <bemonee@gmail.com>
2018-06-09 14:42:34 -0300

    Inicializacion de proyecto

---
+++ src/main/java/com/utn/tp5search/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.utn.tp5search;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Tp5SearchApplication.class);
+	}
+
+}
